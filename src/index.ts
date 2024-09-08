6f763ad93328f86b3b99a1cdbd9cf75bad098c2d
Dinuda Yaggahavita <63149008+Dinuda@users.noreply.github.com>
2024-09-08 19:20:50 +0530

    initialize npm package

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// src/index.ts
+
+// Import CSS
+import "./assets/styles.css";
+
+// Import utility functions
+import { loadTranscriptTracer, cleanupTranscriptTracer } from "./utils";
+
+// Export necessary functions
+export { loadTranscriptTracer, cleanupTranscriptTracer };
